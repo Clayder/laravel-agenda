74f43d6bf1541abe89e0a19daaaca0f27ad63db3
Peter Clayder <peterclayder@gmail.com>
2019-06-29 23:00:08 -0300

    :art: Fatiei o layout usando o blade.
    issues: #17

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,29 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{csrf_token()}}">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Contatos</title>
+@extends("app")
 
-    <!-- Bootstrap -->
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/683d3262aa.js"></script>
-    <script type="text/javascript"> (function() { var css = document.createElement('link'); css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css'; css.rel = 'stylesheet'; css.type = 'text/css'; document.getElementsByTagName('head')[0].appendChild(css); })(); </script>
+@section("titulo", "Editar contato")
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body>
-<div class="container" style="margin-top: 30px; padding: 20px">
+@section("conteudo")
     <div class="row">
         <div class="panel panel-default">
             <div class="panel-heading">
@@ -31,7 +10,8 @@
             </div>
             <div class="panel-body">
                 @if(session('msg') || session('msgError'))
-                    <div class="alert alert-{{ session('msg') ? "success" : "danger" }} alert-dismissible fade in" role="alert">
+                    <div class="alert alert-{{ session('msg') ? "success" : "danger" }} alert-dismissible fade in"
+                         role="alert">
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">×</span>
                         </button>
@@ -44,22 +24,26 @@
                     <input type="hidden" name="id" value="{{$contato->id}}">
                     <div class="form-group {{ $errors->has('nome') ? 'has-error' : ''}}">
                         <label class="control-label">Nome <span class="text-danger">*</span></label>
-                        <input type="text" class="form-control" name="nome" value="{{ old('nome') ? old('nome') : $contato->nome }}">
+                        <input type="text" class="form-control" name="nome"
+                               value="{{ old('nome') ? old('nome') : $contato->nome }}">
                         <span class="help-block">{{ $errors->first('nome') }}</span>
                     </div>
                     <div class="form-group {{ $errors->has('telefone') ? 'has-error' : ''}}">
                         <label class="control-label">Telefone <span class="text-danger">*</span></label>
-                        <input type="text" class="form-control" name="telefone" value="{{ old('telefone') ? old('telefone') : $contato->telefone }}">
+                        <input type="text" class="form-control" name="telefone"
+                               value="{{ old('telefone') ? old('telefone') : $contato->telefone }}">
                         <span class="help-block">{{ $errors->first('telefone') }}</span>
                     </div>
                     <div class="form-group {{ $errors->has('email') ? 'has-error' : ''}}">
                         <label class="control-label">E-mail <span class="text-danger">*</span></label>
-                        <input type="email" class="form-control" name="email" value="{{ old('email') ? old('email') : $contato->email }}">
+                        <input type="email" class="form-control" name="email"
+                               value="{{ old('email') ? old('email') : $contato->email }}">
                         <span class="help-block">{{ $errors->first('email') }}</span>
                     </div>
                     <div id="div-cep" class="form-group div-endereco {{ $errors->has('cep') ? 'has-error' : ''}}">
                         <label class="control-label">Cep <span class="text-danger">*</span></label>
-                        <input type="text" class="form-control" id="cep" name="cep" value="{{ old('cep') ? old('cep') : $contato->cep }}">
+                        <input type="text" class="form-control" id="cep" name="cep"
+                               value="{{ old('cep') ? old('cep') : $contato->cep }}">
                         <span class="help-block msg-endereco" id="msg-cep">{{ $errors->first('cep') }}</span>
                     </div>
                     <div class="form-group div-endereco {{ $errors->has('logradouro') ? 'has-error' : ''}}">
@@ -68,7 +52,8 @@
                             <span class="text-danger">*</span>
                             <span class="fa fa-spinner fa-spin spinner-endereco" style="display: none"></span>
                         </label>
-                        <input type="text" class="form-control" id="rua" name="logradouro" value="{{ old('logradouro') ? old('logradouro') : $contato->logradouro }}">
+                        <input type="text" class="form-control" id="rua" name="logradouro"
+                               value="{{ old('logradouro') ? old('logradouro') : $contato->logradouro }}">
                         <span class="help-block msg-endereco">{{ $errors->first('logradouro') }}</span>
                     </div>
                     <div class="form-group div-endereco">
@@ -76,7 +61,8 @@
                             Complemento
                             <span class="fa fa-spinner fa-spin spinner-endereco" style="display: none"></span>
                         </label>
-                        <input type="text" class="form-control" id="complemento" name="complemento" value="{{ old('complemento') ? old('complemento') : $contato->complemento }}">
+                        <input type="text" class="form-control" id="complemento" name="complemento"
+                               value="{{ old('complemento') ? old('complemento') : $contato->complemento }}">
                         <span class="help-block msg-endereco">{{ $errors->first('complemento') }}</span>
                     </div>
                     <div class="form-group div-endereco {{ $errors->has('bairro') ? 'has-error' : ''}}">
@@ -85,7 +71,8 @@
                             <span class="text-danger">*</span>
                             <span class="fa fa-spinner fa-spin spinner-endereco" style="display: none"></span>
                         </label>
-                        <input type="text" class="form-control" id="bairro" name="bairro" value="{{ old('bairro') ? old('bairro') : $contato->bairro }}">
+                        <input type="text" class="form-control" id="bairro" name="bairro"
+                               value="{{ old('bairro') ? old('bairro') : $contato->bairro }}">
                         <span class="help-block msg-endereco">{{ $errors->first('bairro') }}</span>
                     </div>
                     <div class="form-group div-endereco {{ $errors->has('localidade') ? 'has-error' : ''}}">
@@ -94,7 +81,8 @@
                             <span class="text-danger">*</span>
                             <span class="fa fa-spinner fa-spin spinner-endereco" style="display: none"></span>
                         </label>
-                        <input type="text" class="form-control" id="cidade" name="localidade" value="{{ old('localidade') ? old('localidade') : $contato->localidade }}">
+                        <input type="text" class="form-control" id="cidade" name="localidade"
+                               value="{{ old('localidade') ? old('localidade') : $contato->localidade }}">
                         <span class="help-block msg-endereco">{{ $errors->first('localidade') }}</span>
                     </div>
                     <div class="form-group div-endereco {{ $errors->has('uf') ? 'has-error' : ''}}">
@@ -103,31 +91,29 @@
                             <span class="text-danger">*</span>
                             <span class="fa fa-spinner fa-spin spinner-endereco" style="display: none"></span>
                         </label>
-                        <input type="text" class="form-control" id="uf" name="uf" value="{{ old('uf') ? old('uf') : $contato->uf }}">
+                        <input type="text" class="form-control" id="uf" name="uf"
+                               value="{{ old('uf') ? old('uf') : $contato->uf }}">
                         <span class="help-block msg-endereco">{{ $errors->first('uf') }}</span>
                     </div>
 
-                    <button type="submit" class="btn btn-success" id="btn-submit"><i class="fas fa-edit"></i> Editar </button>
+                    <button type="submit" class="btn btn-success" id="btn-submit"><i class="fas fa-edit"></i> Editar
+                    </button>
                 </form>
 
                 <form action="/contato/delete" method="POST" onsubmit="return confirm('Realmente deseja excluir ?');">
                     @csrf
                     @method('DELETE')
                     <input type="hidden" name="id" value="{{$contato->id}}">
-                    <button type="submit" class="btn btn-danger" id="btn-submit"><i class="fas fa-trash-alt"></i> Excluir </button>
+                    <button type="submit" class="btn btn-danger" id="btn-submit"><i class="fas fa-trash-alt"></i>
+                        Excluir
+                    </button>
                 </form>
 
             </div>
         </div>
     </div>
-</div>
+@endsection
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-        crossorigin="anonymous"></script>
-<script src="{{asset('js/App/ViaCep.js')}}" type="text/javascript"></script>
-</body>
-</html>
+@section("js")
+    <script src="{{asset('js/App/ViaCep.js')}}" type="text/javascript"></script>
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{csrf_token()}}">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Contatos</title>
+@extends("app")
 
-    <!-- Bootstrap -->
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/683d3262aa.js"></script>
-    <script type="text/javascript"> (function() { var css = document.createElement('link'); css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css'; css.rel = 'stylesheet'; css.type = 'text/css'; document.getElementsByTagName('head')[0].appendChild(css); })(); </script>
+@section("titulo", "Contatos")
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body>
-<div class="container" style="margin-top: 30px; padding: 20px">
+@section("conteudo")
     <div class="row">
         <a href="/contato/create" class="btn btn-success btn-lg" style="float: right">
             <i class="fas fa-user-plus"></i>
@@ -94,16 +73,10 @@
             </ul>
         </nav>
     </div>
-</div>
+    </div>
+@endsection
 
+@section("js")
+    <script src="{{asset('js/App/index.js')}}" type="text/javascript"></script>
+@endsection
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-        crossorigin="anonymous"></script>
-
-<script src="{{asset('js/App/index.js')}}" type="text/javascript"></script>
-</body>
-</html>
