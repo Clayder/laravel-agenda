9dc00b1d0a49d18465aaa38dd63533053fa23817
Peter Clayder <peterclayder@gmail.com>
2019-06-30 14:17:42 -0300

    :lock: Bloqueio rota home.
    issues: #21

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \Illuminate\Routing\Redirector;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return redirect('/');
     }
 }
