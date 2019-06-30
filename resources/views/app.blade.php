8d8cd3bab8ec8c6965b5ecafc4683ef571ccaaf4
Peter Clayder <peterclayder@gmail.com>
2019-06-30 10:15:40 -0300

    :heavy_plus_sign: Menu do topo
    issues: #17

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,7 +32,20 @@
 <body>
 <div class="container" style="margin-top: 30px; padding: 20px">
     @section("menu-topo")
-        {{--menu--}}
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/"><i class="fas fa-address-book text-primary"></i> <span class="text-primary">Contatos</span></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
     @show
     @yield("conteudo")
 </div>
