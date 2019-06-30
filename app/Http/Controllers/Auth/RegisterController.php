4387697f557c976f86fd5c437c9128c9b46ae9d7
Peter Clayder <peterclayder@gmail.com>
2019-06-30 13:47:30 -0300

    :heavy_plus_sign: Protegendo a rota register.
    issues: #21

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -39,7 +39,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('auth');
     }
 
     /**
